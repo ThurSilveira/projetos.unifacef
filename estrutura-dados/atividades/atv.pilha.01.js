abaa8a3acec5a8c36e7c217b50df6a05f2028533
ThurSilveira <arthursilveira13000@gmail.com>
2026-03-22 10:51:39 -0300

    atv.1.ok

--- estrutura-dados/atividades/atv.pilha.01.js
+++ estrutura-dados/atividades/atv.pilha.01.js
@@ -1,28 +1,79 @@
-import Stack from "lib/Stack.js";
+import Stack from "../lib/Stack.js";
 
 let pilha = new Stack();
-// Estado inicial: []
 
-pilha.push(5);    // Adiciona 5 no topo: [5]
-pilha.push(9);    // Adiciona 9 no topo: [5, 9]
-pilha.pop();      // Remove o último elemento (9): [5]
-pilha.push(3);    // Adiciona 3 no topo: [5, 3]
-pilha.push(8);    // Adiciona 8 no topo: [5, 3, 8]
-pilha.pop();      // Remove o último elemento (8): [5, 3]
-pilha.pop();      // Remove o último elemento (3): [5]
-pilha.push(6);    // Adiciona 6 no topo: [5, 6]
+// estado inicial
+console.log(pilha.print()); // []
+console.log("Está vazia?\n", pilha.isEmpty); // true
 
+// push(5)
+pilha.push(5);
+console.log(pilha.print()); // [5]
 
-pilha.push(2);    // Adiciona 2 no topo: [5, 6, 2]
-pilha.pop();      // Remove o último elemento (2): [5, 6]
-pilha.push(10);   // Adiciona 10 no topo: [5, 6, 10]
-pilha.push(1);    // Adiciona 1 no topo: [5, 6, 10, 1]
-pilha.pop();      // Remove o último elemento (1): [5, 6, 10]
-pilha.pop();      // Remove o último elemento (10): [5, 6]
-pilha.push(15);   // Adiciona 15 no topo: [5, 6, 15]
-pilha.pop();      // Remove o último elemento (15): [5, 6]
-pilha.pop();      // Remove o último elemento (6): [5]
+// push(9)
+pilha.push(9);
+console.log(pilha.print()); // [5,9]
 
-// Verificação final
-console.log(pilha.print()); // Saída: [5]
-console.log("O topo atual é:", pilha.peek()); // Saída: 5
+// pop()
+pilha.pop();
+console.log(pilha.print()); // [5]
+
+// push(3)
+pilha.push(3);
+console.log(pilha.print()); // [5,3]
+
+// push(8)
+pilha.push(8);
+console.log(pilha.print()); // [5,3,8]
+
+// pop()
+pilha.pop();
+console.log(pilha.print()); // [5,3]
+
+// pop()
+pilha.pop();
+console.log(pilha.print()); // [5]
+
+// push(6)
+pilha.push(6);
+console.log(pilha.print()); // [5,6]
+
+// push() -> undefined
+pilha.push();
+console.log(pilha.print()); // [5,6,null]
+
+// push(2)
+pilha.push(2);
+console.log(pilha.print()); // [5,6,null,2]
+
+// pop()
+pilha.pop();
+console.log(pilha.print()); // [5,6,null]
+
+// push(10)
+pilha.push(10);
+console.log(pilha.print()); // [5,6,null,10]
+
+// push(1)
+pilha.push(1);
+console.log(pilha.print()); // [5,6,null,10,1]
+
+// pop()
+pilha.pop();
+console.log(pilha.print()); // [5,6,null,10]
+
+// pop()
+pilha.pop();
+console.log(pilha.print()); // [5,6,null]
+
+// push(15)
+pilha.push(15);
+console.log(pilha.print()); // [5,6,null,15]
+
+// pop()
+pilha.pop();
+console.log(pilha.print()); // [5,6,null]
+
+// pop()
+pilha.pop();
+console.log(pilha.print()); // [5,6]
